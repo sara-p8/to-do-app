64d053f60d6abc5165abacbfde17a50efa1c6563
Sara <sarapapa308@gmail.com>
2019-07-23 02:22:56 -0700

    Checkpoint 11.13 DOM Scripting Pt 2 Bonus: Complete

--- app.js
+++ app.js
@@ -1,46 +1,74 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  let storageThings = localStorage.getItem('toDoStorage');
+  let id = 0;
+  let toDos = [];
+  if (storageThings != null) {
+    toDos = JSON.parse(storageThings);
+    for (var i = 0; i < toDos.length ; i++) {
+      if (toDos[i].id > id) {
+        id = toDos[i].id;
+      }
+    }
+  }
+
+   const addToDoForm = document.getElementById('addToDoForm');
+
+   function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+      if (!newToDoText.value) { return; }
+      toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: ++id,
+    });
+      newToDoText.value = '';
+
+      renderTheUI();
+  }
 
   addToDoForm.addEventListener('submit', event => {
-
     event.preventDefault();
-
-    // get the text
-    let title = newToDoText.value;
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // create delete button
-    let deleteButton = document.createElement('button');
-        deleteButton.textContent = "Delete";
-        deleteButton.addEventListener('click', function(event) {
-          toDoList.removeChild(this.parentElement);
-        });
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // delete button
-    newLi.appendChild(deleteButton);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
-    newToDoText.value = '';
-
+    createNewToDo();
   });
+
+  function renderTheUI() {
+    let toDoList = document.getElementById('toDoList');
+
+      toDoList.textContent = '';
+
+      toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      let checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      checkbox.checked = toDo.complete;
+
+      checkbox.addEventListener('change', function() {
+        if(this.checked) {
+          toDo.complete = true;
+        } else {
+          toDo.complete = false;
+        }
+        localStorage.setItem('toDoStorage', JSON.stringify(toDos));
+      })
+
+      let deleteButton = document.createElement ('button');
+      deleteButton.textContent = "Delete";
+
+      deleteButton.addEventListener('click', event => {
+      toDos = toDos.filter(function(item){
+        return item.id !== toDo.id;
+      })
+        renderTheUI();
+      });
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+
+    });
+}
 }
 
 window.onload = function() {
